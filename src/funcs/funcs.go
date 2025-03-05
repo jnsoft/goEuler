87cccb2062c7c687f671feca1fd906978c877508
Johan <jnsoft@users.noreply.github.com>
2025-03-05 22:40:29 +0000

    ok

--- src/funcs/funcs.go
+++ src/funcs/funcs.go
@@ -8,3 +8,15 @@ func Hello(name string) string {
 	message := fmt.Sprintf("Hi, %v. Welcome!", name)
 	return message
 }
+
+func Multiples_of_3_and_5(max int) int {
+	sum := 0
+	for i := 1; i < max; i++ {
+		if i%3 == 0 {
+			sum += i
+		} else if i%5 == 0 {
+			sum += i
+		}
+	}
+	return sum
+}
--- src/main.go
+++ src/main.go
@@ -2,23 +2,37 @@ package main
 
 import (
 	"fmt"
+	"time"
 
-	"github.com/jnsoft/goEuler/src/funcs"
+	. "github.com/jnsoft/goEuler/src/funcs"
 	"github.com/jnsoft/jngo/bag"
 )
 
 func main() {
 	// Get a greeting message and print it.
-	message := funcs.Hello("Gladys")
+	message := Hello("Johan")
 	fmt.Println(message)
 
 	b := bag.NewBag[int]()
 
 	b.Add(4)
 	b.Add(3)
-	b.Add(3)
 
-	fmt.Println(b.Count(3))
-	fmt.Println(b.Count(2))
+	timeFunction("Answer 1", func() (interface{}, error) {
+		return Multiples_of_3_and_5(1000), nil
+	})
 
+}
+
+func timeFunction(label string, f func() (interface{}, error)) {
+	start := time.Now()
+	result, err := f()
+	elapsed := time.Since(start)
+
+	if err != nil {
+		fmt.Printf("Error: %v\n", err)
+		return
+	}
+
+	fmt.Printf("%s: %v (%s)\n", label, result, elapsed)
 }
