8512755f3e5d596e2dbbb609163c51531cc99d2a
Johan <jnsoft@users.noreply.github.com>
2025-03-05 22:08:03 +0000

    ok

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/jnsoft/goEuler
 
 go 1.22.2
+
+require github.com/jnsoft/jngo v0.0.0-20250202153851-ae296cecc84a
--- src/main.go
+++ src/main.go
@@ -4,10 +4,21 @@ import (
 	"fmt"
 
 	"github.com/jnsoft/goEuler/src/funcs"
+	"github.com/jnsoft/jngo/bag"
 )
 
 func main() {
 	// Get a greeting message and print it.
 	message := funcs.Hello("Gladys")
 	fmt.Println(message)
+
+	b := bag.NewBag[int]()
+
+	b.Add(4)
+	b.Add(3)
+	b.Add(3)
+
+	fmt.Println(b.Count(3))
+	fmt.Println(b.Count(2))
+
 }
